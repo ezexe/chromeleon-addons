35aa2a4a71e8f190a95017e4ded83873f5669cfc
ezexe <elimdadia@gmail.com>
2024-09-26 16:39:27 +0300

    clientrects

--- src/Chameleon.app.Addons/Assets/addons/foxameleon/modules/spoof/clientrects.js
+++ src/Chameleon.app.Addons/Assets/addons/foxameleon/modules/spoof/clientrects.js
@@ -1,117 +1,117 @@
-const { settings } = require('./settings.js');
+const { settings } = require("./modules/settings.js");
+const { log } = require("./modules/logger.js");
 
-const rectspoof = {
-  clientRects: {
-    noise: {
-      DOMRect: 0.00000001,
-      DOMRectReadOnly: 0.000001,
-      low: 0.3,
-      medium: 0.5,
-      high: 0.8,
-    },
-    metrics: {
-      DOMRect: ["x", "y", "width", "height"],
-      DOMRectReadOnly: ["top", "right", "bottom", "left"],
-    },
-    method: {
-      DOMRect: function (e) {
-        try {
-          Object.defineProperty(DOMRect.prototype, e, {
-            get: new Proxy(
-              Object.getOwnPropertyDescriptor(DOMRect.prototype, e).get,
-              {
-                get(target, p, receiver) {
-                  return target;
-                },
-                apply(target, self, args) {
-                  const result = Reflect.apply(target, self, args);
-                  if (!settings.enabled || !settings.clientRectsSpoofing)
-                    return result;
-                  //
-                  const _result =
-                    result *
-                    (1 +
-                      (Math.random() <
-                      rectspoof.clientRects.noise[settings.noiseLevel]
-                        ? -1
-                        : +1) *
-                        rectspoof.clientRects.noise.DOMRect);
-                  return _result;
-                },
-              }
-            ),
-          });
-          //
-          Object.defineProperty(DOMRect.prototype, e, {
-            get: Object.getOwnPropertyDescriptor(DOMRect.prototype, e).get,
-          });
-        } catch (e) {
-          log.error(e);
-        }
-      },
-      DOMRectReadOnly: function (e) {
-        try {
-          Object.defineProperty(DOMRectReadOnly.prototype, e, {
-            get: new Proxy(
-              Object.getOwnPropertyDescriptor(
-                DOMRectReadOnly.prototype,
-                e
-              ).get,
-              {
-                get(target, p, receiver) {
-                  return target;
-                },
-                apply(target, self, args) {
-                  const result = Reflect.apply(target, self, args);
-                  if (!settings.enabled || !settings.clientRectsSpoofing)
-                    return result;
-                  //
-                  const _result =
-                    result *
-                    (1 +
-                      (Math.random() <
-                      rectspoof.clientRects.noise[settings.noiseLevel]
-                        ? -1
-                        : +1) *
-                        rectspoof.clientRects.noise.DOMRectReadOnly);
-                  return _result;
-                },
-              }
-            ),
-          });
-          //
-          Object.defineProperty(DOMRectReadOnly.prototype, e, {
-            get: Object.getOwnPropertyDescriptor(DOMRectReadOnly.prototype, e)
-              .get,
-          });
-        } catch (e) {
-          log.error(e);
-        }
-      },
-    },
+const config = {
+  noise: {
+    DOMRect: 0.00000001,
+    DOMRectReadOnly: 0.000001,
+    low: 0.3,
+    medium: 0.5,
+    high: 0.8,
+  },
+  metrics: {
+    DOMRect: ["x", "y", "width", "height"],
+    DOMRectReadOnly: ["top", "right", "bottom", "left"],
+  },
+};
+const methods = {
+  DOMRect: async function (e) {
+    await browser.tabs.executeScript(tab.id, {
+      code: ` (function() {
+        Object.defineProperty(DOMRect.prototype, e, {
+          get: new Proxy(
+            Object.getOwnPropertyDescriptor(DOMRect.prototype, e).get,
+            {
+              get(target, p, receiver) {
+                return target;
+              },
+              apply(target, self, args) {
+                const result = Reflect.apply(target, self, args);
+                const _result =
+                  result *
+                  (1 +
+                    (Math.random() <
+                    ${config.noise[settings.noiseLevel]}
+                      ? -1
+                      : +1) *
+                      ${config.noise.DOMRect});
+                return _result;
+              },
+            }
+          ),
+        });
+        Object.defineProperty(DOMRect.prototype, e, {
+          get: Object.getOwnPropertyDescriptor(DOMRect.prototype, e).get,
+        });  
+        })();
+        `,
+      runAt: "document_start",
+    });
+  },
+  DOMRectReadOnly: async function (e) {
+    await browser.tabs.executeScript(tab.id, {
+      code: `(function() {
+        Object.defineProperty(DOMRectReadOnly.prototype, e, {
+          get: new Proxy(
+            Object.getOwnPropertyDescriptor(DOMRectReadOnly.prototype, e).get,
+            {
+              get(target, p, receiver) {
+                return target;
+              },
+              apply(target, self, args) {
+                const result = Reflect.apply(target, self, args);
+                const _result =
+                  result *
+                  (1 +
+                    (Math.random() <
+                    ${config.noise[settings.noiseLevel]}
+                      ? -1
+                      : +1) *
+                      ${config.noise.DOMRectReadOnly});
+                return _result;
+              },
+            }
+          ),
+        });
+        Object.defineProperty(DOMRectReadOnly.prototype, e, {
+          get: Object.getOwnPropertyDescriptor(DOMRectReadOnly.prototype, e).get,
+        });
+      })();
+        `,
+      runAt: "document_start",
+    });
   },
 };
 
-const clientRectsSpoofing = {
-  apply: function () {
-    if (settings.clientRectsSpoofing) {
-      // Spoofing of DOMRect
-      {
-        const metrics = ["x", "y", "width", "height"];
-        for (let i = 0; i < metrics.length; i++) {
-          rectspoof.clientRects.method.DOMRect(metrics[i]);
-        }
-      }
-
-      // Spoofing of DOMRectReadOnly
-      {
-        const metrics = ["top", "right", "bottom", "left"];
-        for (let i = 0; i < metrics.length; i++) {
-          rectspoof.clientRects.method.DOMRectReadOnly(metrics[i]);
-        }
-      }
+async function applyClientRectsSpoofing(tab) {
+  if (tab.url.indexOf("about:") < 0 && settings.clientRectsSpoofing) {
+    log.info(`Applying applyClientRectsSpoofing spoofing for tab ${tab.id}`);
+    try {
+      methods.DOMRect();
+      methods.DOMRectReadOnly();
+    } catch (error) {
+      log.error(`Failed to apply canvas spoofing for tab ${tab.id}:`, error);
     }
   }
+}
+
+function setupTabListeners() {
+  browser.tabs.onCreated.addListener((tab) => {
+    applyClientRectsSpoofing(tab);
+  });
+
+  browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === "loading") {
+      applyClientRectsSpoofing(tab);
+    }
+  });
+}
+
+// Initialize
+setupTabListeners();
+
+module.exports = {
+  applyClientRectsSpoofing,
 };
 
-module.exports = clientRectsSpoofing;
+module.exports = applyClientRectsSpoofing;
